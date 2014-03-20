1a86d7d525b6bd482ec7a2248349680177a36320
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-20 11:45:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birdhaus::Application.config.secret_key_base = 'c13977426ad1d450640c65a66e95edfa253456a24069ee240d2a729d7cabd2478c5b7385dfa026d5f1a00abf9c2069bd7f926255c46576b012bb6f9946fb5a77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Birdhaus::Application.config.session_store :cookie_store, key: '_birdhaus_session'
