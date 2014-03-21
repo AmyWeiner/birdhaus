6d54eb92bc437ff95b64d3a284e1547e67de692a
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-21 11:14:45 -0700

    Starting work on getting app to respond to JSON inputs

--- app/controllers/birdhaus_controller.rb
+++ app/controllers/birdhaus_controller.rb
@@ -1,6 +1,28 @@
 class BirdhausController < ApplicationController
+    respond_to :json
     
     def index
+        post_data = request.body.read
+        req = JSON.parse(post_data)
     end
+
+    def create
+        @temp1 = Temperature1s.new(params[temp_params])
+        if @temp1.save
+            format.json {render :json => @temp1.as_json}
+            return
+        end
+    end
+
+    def temp_params
+        params.permit(:temp, :time)
+    end
+    
+    # def raw_post(action, params, body)
+    #     @request.env['RAW_POST_DATA'] = body
+    #     response = post(action, params)
+    #     @request.env.delete('RAW_POST_DATA')
+    #     response
+    # end
 
 end
