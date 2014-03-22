15f032c0d2109b2d55c370750d7a4102fac0439e
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-21 17:23:01 -0700

    Reformatted respond_to code in controller

--- app/controllers/birdhaus_controller.rb
+++ app/controllers/birdhaus_controller.rb
@@ -1,17 +1,24 @@
 class BirdhausController < ApplicationController
     
     def index
+        @temp1 = Temperature1.all
+        respond_to do |format|
+            format.html
+            format.json {render json: @temp1}
+        end
         # post_data = request.body.read
         # req = JSON.parse(post_data)
     end
 
     def create
-        @temp1 = Temperature1s.new(params[temp_params])
+        @temp1 = Temperature1.new(params[temp_params])
         if @temp1.save
             respond_to do |format|
-                format.json {render :json => @temp1.to_json}
+                format.html
+                format.json {render json: @temp1}
             end
         end
+        redirect_to root_url
     end
 
     def temp_params
