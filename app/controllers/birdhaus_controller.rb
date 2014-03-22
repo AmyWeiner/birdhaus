cb2e2268857dac016b01762b7fb65231c149efd7
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-21 19:30:38 -0700

    Able to save JSON to localhost from own curl command but params strange

--- app/controllers/birdhaus_controller.rb
+++ app/controllers/birdhaus_controller.rb
@@ -1,18 +1,17 @@
 class BirdhausController < ApplicationController
     skip_before_filter :verify_authenticity_token
-    
+
     def index
         @temp1 = Temperature1.all
         respond_to do |format|
             format.html
             format.json {render json: @temp1}
         end
-        # post_data = request.body.read
-        # req = JSON.parse(post_data)
     end
 
     def create
-        @temp1 = Temperature1.new(params[temp_params])
+        temp1 = params.require(:birdhau).permit(:temp, :time)
+        @temp1 = Temperature1.create(temp1)
         if @temp1.save
             respond_to do |format|
                 format.html
@@ -21,9 +20,9 @@ class BirdhausController < ApplicationController
         end
     end
 
-    def temp_params
-        params.permit(:temp, :time)
-    end
+    # def temp_params
+    #     params.permit(:temp, :time)
+    # end
 
     # def raw_post(action, params, body)
     #     @request.env['RAW_POST_DATA'] = body
