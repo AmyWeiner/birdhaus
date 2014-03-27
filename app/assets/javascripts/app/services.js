91e4b53302967fa9b38cad4b1cf09781640b3770
Amy Weiner <arweiner@alumni.stanford.edu>
2014-03-26 22:25:39 -0700

    deleted unused file

--- app/assets/javascripts/app/services.js
+++
@@ -1,5 +0,0 @@
-// angular.module('birdhaus.services', []);
-//     birdhaus.factory('tempRecords', function() {
-//         var thingerMajig = somethingOrOther;
-//         return somethingOrOther;
-//     });
